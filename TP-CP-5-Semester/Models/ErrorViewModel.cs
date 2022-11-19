8e08a0f3f2c6229817db6e5f817443e80dccb459
IlyaMuntyanu <muntyany.ilya8@gmail.com>
2022-11-19 11:55:40 +0300

    добавлен сгенерированный шаблон

---
+++ TP-CP-5-Semester/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP_CP_5_Semester.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
