29a398654c0517bab8d188885451aa4994fa6eb7
IlyaMuntyanu <muntyany.ilya8@gmail.com>
2022-12-04 11:07:09 +0300

    зарегистрирован контекст бд

--- TP-CP-5-Semester/Program.cs
+++ TP-CP-5-Semester/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using TP_CP_5_Semester.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("Default");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
 
 var app = builder.Build();
 if (!app.Environment.IsDevelopment())
