b7b72ab43e0398b00ab754d906905e1b65fc895d
IlyaMuntyanu <muntyany.ilya8@gmail.com>
2022-12-13 08:55:03 +0300

    внедрен контекст бд в контроллер

--- TP-CP-5-Semester/Controllers/HomeController.cs
+++ TP-CP-5-Semester/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using TP_CP_5_Semester.Data;
 using TP_CP_5_Semester.Models;
 
 namespace TP_CP_5_Semester.Controllers;
@@ -7,10 +8,12 @@ namespace TP_CP_5_Semester.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _db;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
     {
         _logger = logger;
+        _db = db;
     }
 
     public IActionResult Index()
