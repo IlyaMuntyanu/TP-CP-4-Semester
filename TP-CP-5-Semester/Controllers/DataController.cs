ae81c0499fac2c364cff359dedbe241dee541181
IlyaMuntyanu <muntyany.ilya8@gmail.com>
2022-12-14 22:36:48 +0300

    добавлен контроллер для получения данных из бд

---
+++ TP-CP-5-Semester/Controllers/DataController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TP_CP_5_Semester.Data;
+using TP_CP_5_Semester.Models;
+
+namespace TP_CP_5_Semester.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class DataController: ControllerBase
+{
+    private readonly ApplicationDbContext _db;
+
+    public DataController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    [HttpGet("Tour/{id}")]
+    public async Task<Tour> GetTourById(long id)
+    {
+        return await _db.Tours.Where(tour => tour.Id == id).FirstAsync();
+    }
+}
