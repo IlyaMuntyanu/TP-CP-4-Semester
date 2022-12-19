e3262e8dbeae7c79009c496cc6c6f5733210e399
IlyaMuntyanu <muntyany.ilya8@gmail.com>
2022-12-19 20:10:20 +0300

    изменен тип данных идшника

--- TP-CP-5-Semester/RequestBodies/OrderRequest.cs
+++ TP-CP-5-Semester/RequestBodies/OrderRequest.cs
@@ -3,6 +3,6 @@ namespace TP_CP_5_Semester.RequestBodies;
 public class OrderRequest
 {
     public string Email { get; set; }
-    public long TourId { get; set; }
+    public int TourId { get; set; }
     public int Amount { get; set; }
 }
