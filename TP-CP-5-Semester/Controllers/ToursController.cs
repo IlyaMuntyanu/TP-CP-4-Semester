b0281138417107bc2f481d24d3a8eb70a3fa15bf
IlyaMuntyanu <muntyany.ilya8@gmail.com>
2022-12-20 17:21:42 +0300

    добавлена загрузка данных о броне путевок

--- TP-CP-5-Semester/Controllers/ToursController.cs
+++ TP-CP-5-Semester/Controllers/ToursController.cs
@@ -1,11 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TP_CP_5_Semester.Data;
 
 namespace TP_CP_5_Semester.Controllers;
 
 public class ToursController : Controller
 {
-    public IActionResult Index()
+    private readonly ApplicationDbContext _db;
+
+    public ToursController(ApplicationDbContext db)
     {
+        _db = db;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        if (User.Identity.IsAuthenticated)
+        {
+            ViewBag.BookingsList = await _db.Bookings.Where(b => b.User.Email == User.Identity.Name)
+                .Include(b => b.Tour).ToListAsync();
+            ViewBag.UserBalance = (await _db.Users.Where(u => u.Email == User.Identity.Name).FirstAsync()).Balance;
+        }
+
         return View();
     }
 }
