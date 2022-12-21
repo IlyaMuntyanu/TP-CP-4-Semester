7629bbcc0b298b813e7440ea1233b7ed4d7f1d1c
IlyaMuntyanu <muntyany.ilya8@gmail.com>
2022-12-21 10:54:43 +0300

    убрана автоматическая оплата путевки

--- TP-CP-5-Semester/Controllers/HomeController.cs
+++ TP-CP-5-Semester/Controllers/HomeController.cs
@@ -52,8 +52,6 @@ public class HomeController : Controller
         var bookingStatus = await _db.BookingStatuses.Where(bs => bs.Name.Equals("Забронировано")).FirstAsync();
         var tour = await _db.Tours.FindAsync(body.TourId);
         ArgumentNullException.ThrowIfNull(tour);
-        
-        user.Balance -= tour.Price * body.Amount;
 
         var booking = new Booking
         {
