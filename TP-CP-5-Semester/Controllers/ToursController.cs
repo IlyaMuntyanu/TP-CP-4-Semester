35483892d30a5b12967c4954061255f9db65dab8
IlyaMuntyanu <muntyany.ilya8@gmail.com>
2022-12-21 18:20:29 +0300

    рефактор

--- TP-CP-5-Semester/Controllers/ToursController.cs
+++ TP-CP-5-Semester/Controllers/ToursController.cs
@@ -15,13 +15,13 @@ public class ToursController : Controller
 
     public async Task<IActionResult> Index()
     {
-        if (User.Identity.IsAuthenticated)
-        {
-            ViewBag.BookingsList = await _db.Bookings.Where(b => b.User.Email == User.Identity.Name)
-                .Include(b => b.Tour)
-                .Include(b => b.Status).ToListAsync();
-            ViewBag.UserBalance = (await _db.Users.Where(u => u.Email == User.Identity.Name).FirstAsync()).Balance;
-        }
+        if (!User.Identity.IsAuthenticated) return View();
+        
+        ViewBag.BookingsList = await _db.Bookings.Where(b => b.User.Email == User.Identity.Name)
+            .Include(b => b.Tour)
+            .Include(b => b.Status)
+            .ToListAsync();
+        ViewBag.UserBalance = (await _db.Users.Where(u => u.Email == User.Identity.Name).FirstAsync()).Balance;
 
         return View();
     }
