2b26910b9281fa0dad165d06a4d4f9626a9289ea
IlyaMuntyanu <muntyany.ilya8@gmail.com>
2022-12-22 11:44:26 +0300

    добавлено отображение статистики

--- TP-CP-5-Semester/Controllers/StatsController.cs
+++ TP-CP-5-Semester/Controllers/StatsController.cs
@@ -1,13 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TP_CP_5_Semester.Data;
+using TP_CP_5_Semester.Models;
 
 namespace TP_CP_5_Semester.Controllers;
 
 public class StatsController : Controller
 {
     private readonly ApplicationDbContext _db;
-    // GET
+
     public StatsController(ApplicationDbContext db)
     {
         _db = db;
@@ -15,9 +16,16 @@ public class StatsController : Controller
 
     public async Task<IActionResult> Index()
     {
-
         if (User.Identity.IsAuthenticated)
-            ViewBag.UserBalance = (await _db.Users.Where(user => user.Email == User.Identity.Name).FirstAsync()).Balance;
+            ViewBag.UserBalance =
+                (await _db.Users.Where(user => user.Email == User.Identity.Name).FirstAsync()).Balance;
+
+        ViewBag.Stats = await _db.Bookings.Include(b => b.Tour).GroupBy(b => b.Tour.Id).Select(b => new Booking
+        {
+            Tour = b.First().Tour,
+            Amount = b.Sum(booking => booking.Amount)
+        }).ToListAsync();
+        
         return View();
     }
 }
