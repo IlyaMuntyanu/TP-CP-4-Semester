d68c4356fa73f8eb14fd77bdc7c0e76a69dc2758
IlyaMuntyanu <muntyany.ilya8@gmail.com>
2022-12-22 11:11:41 +0300

    внедрено отображение баланса

--- TP-CP-5-Semester/Controllers/StatsController.cs
+++ TP-CP-5-Semester/Controllers/StatsController.cs
@@ -1,12 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TP_CP_5_Semester.Data;
 
 namespace TP_CP_5_Semester.Controllers;
 
 public class StatsController : Controller
 {
+    private readonly ApplicationDbContext _db;
     // GET
-    public IActionResult Index()
+    public StatsController(ApplicationDbContext db)
     {
+        _db = db;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+
+        if (User.Identity.IsAuthenticated)
+            ViewBag.UserBalance = (await _db.Users.Where(user => user.Email == User.Identity.Name).FirstAsync()).Balance;
         return View();
     }
 }
