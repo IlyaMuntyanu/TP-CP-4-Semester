ab784461137a2c926a8f9f33e78bc0a6ed784e54
IlyaMuntyanu <muntyany.ilya8@gmail.com>
2022-12-26 13:24:11 +0300

    добавлены модели конфигурации

---
+++ PaymentAPI/Configuration/ApiConfiguration.cs
@@ -0,0 +1,6 @@
+namespace PaymentAPI.Configuration;
+
+public class ApiConfiguration
+{
+    public required SecurityConfiguration Security { get; set; }
+}
---
+++ PaymentAPI/Configuration/SecurityConfiguration.cs
@@ -0,0 +1,6 @@
+namespace PaymentAPI.Configuration;
+
+public class SecurityConfiguration
+{
+    public required string Token { get; set; }
+}
