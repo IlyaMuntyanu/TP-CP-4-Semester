095bb2e31b11c500bd20b85c18b1a967ec0a9631
IlyaMuntyanu <muntyany.ilya8@gmail.com>
2022-12-26 19:28:47 +0300

    добавлено скрытие из ответов api некоторых полей

--- PaymentAPI/Models/Card.cs
+++ PaymentAPI/Models/Card.cs
@@ -1,7 +1,10 @@
+using System.Text.Json.Serialization;
+
 namespace PaymentAPI.Models;
 
 public class Card
 {
+    [JsonIgnore]
     public int Id { get; set; }
     public long CardNumber { get; set; }
     public DateOnly ValidThrough { get; set; }
--- PaymentAPI/Models/CardStatus.cs
+++ PaymentAPI/Models/CardStatus.cs
@@ -1,7 +1,10 @@
+using System.Text.Json.Serialization;
+
 namespace PaymentAPI.Models;
 
 public class CardStatus
 {
+    [JsonIgnore]
     public int Id { get; set; }
     public string Name { get; set; }
 }
