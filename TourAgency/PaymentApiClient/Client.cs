58edaea3406fd950a38161ba5bdb551afcefa5b0
IlyaMuntyanu <muntyany.ilya8@gmail.com>
2022-12-26 22:55:19 +0300

    переписан трансфер

--- TourAgency/PaymentApiClient/Client.cs
+++ TourAgency/PaymentApiClient/Client.cs
@@ -1,30 +1,21 @@
+using System.Text;
+using System.Text.Json;
+using TP_CP_5_Semester.PaymentApiClient.RequestBodies;
+
 namespace TP_CP_5_Semester.PaymentApiClient;
 
 public class Client
 {
     private readonly Uri _baseAddress = new("https://localhost:7264");
 
-    public async void TransferCard(
-        long senderCardNumber,
-        int senderValidThroughMonth,
-        int senderValidThroughYear,
-        int senderCvc,
-        long reciverCardNumber,
-        int sum
-    )
+    public async void TransferCard(TransferBody body)
     {
         using var client = new HttpClient();
         client.BaseAddress = _baseAddress;
-        var body = new FormUrlEncodedContent(new[]
-        {
-            new KeyValuePair<string, string>("fromCardNumber", senderCardNumber.ToString()),
-            new KeyValuePair<string, string>("fromValidThroughMonth", senderValidThroughMonth.ToString()),
-            new KeyValuePair<string, string>("fromValidThroughYear", senderValidThroughYear.ToString()),
-            new KeyValuePair<string, string>("fromCvc", senderCvc.ToString()),
-            new KeyValuePair<string, string>("toCardNumber", reciverCardNumber.ToString()),
-            new KeyValuePair<string, string>("sum", sum.ToString())
-        });
-        var result = await client.PostAsync("/Card/Replenish", body);
+        
+        
+        var content = new StringContent(JsonSerializer.Serialize(body), Encoding.UTF8, "application/json");
+        var result = await client.PostAsync("/Card/Transfer", content);
         var resultContent = await result.Content.ReadAsStringAsync();
     }
 }
