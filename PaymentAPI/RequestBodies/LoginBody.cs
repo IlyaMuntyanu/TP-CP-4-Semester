844c67b66bf9f0bebbfe37ae13eb49631628889d
IlyaMuntyanu <muntyany.ilya8@gmail.com>
2022-12-26 19:34:02 +0300

    удалены неиспользуемые модели

--- PaymentAPI/RequestBodies/LoginBody.cs
+++
@@ -1,7 +0,0 @@
-namespace PaymentAPI.RequestBodies;
-
-public class LoginBody
-{
-    public required string Username { get; init; }
-    public required string Password { get; init; }
-}
--- PaymentAPI/RequestBodies/RegisterBody.cs
+++
@@ -1,7 +0,0 @@
-namespace PaymentAPI.RequestBodies;
-
-public class RegisterBody
-{
-    public required string Username { get; init; }
-    public required string Password { get; init; }
-}
