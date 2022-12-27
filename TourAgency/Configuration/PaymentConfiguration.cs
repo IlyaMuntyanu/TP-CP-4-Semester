c219ee5529a0bff4e1ece4408df70109b3ff0b49
IlyaMuntyanu <muntyany.ilya8@gmail.com>
2022-12-27 16:07:05 +0300

    добавлены модели конфигов для платёжной системы

---
+++ TourAgency/Configuration/PaymentConfiguration.cs
@@ -0,0 +1,9 @@
+namespace TP_CP_5_Semester.Configuration;
+
+public class PaymentConfiguration
+{
+    public long CardNumber;
+    public int ValidThroughMonth;
+    public int ValidThroughYear;
+    public int Cvc;
+}
---
+++ TourAgency/Configuration/TourAgencyConfiguration.cs
@@ -0,0 +1,6 @@
+namespace TP_CP_5_Semester.Configuration;
+
+public class TourAgencyConfiguration
+{
+    public required PaymentConfiguration PaymentConfiguration;
+}
